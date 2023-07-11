e003d89f2d2493a48163d5eecdaa48605a0b2b89
MatheusGrando <mthgrando@gmail.com>
2023-07-11 18:33:49 -0300

    Footer e testes feitos.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Profile() {
   return (
     <div>
       <Header haveBar={ false } title="Profile" />
+      <Footer />
     </div>
   );
 }
