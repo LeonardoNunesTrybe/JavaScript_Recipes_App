b349e0a0f290c51dd0948f645146c17e9bd34c78
Jenifer <jnf.asm@gmail.com>
2023-07-16 13:19:03 +0100

    Profile: req 57-62 faltando testes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,53 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Profile() {
+  const history = useHistory();
   const recoveryEmail = JSON.parse(localStorage.getItem('user'));
+
+  const handleClickDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+  const handleClickFavoriteRecipes = () => {
+    history.push('/favorite-recipes');
+  };
+  const handleClickLogout = () => {
+    localStorage.removeItem('user');
+    history.push('/');
+  };
+
   return (
     <div>
       <Header haveBar={ false } title="Profile" />
       <div>
-        <p data-testid="profile-email">{Object.values(recoveryEmail)}</p>
-        <button data-testid="profile-done-btn">Done Recipes</button>
-        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button data-testid="profile-logout-btn">Logout</button>
+        <p
+          data-testid="profile-email"
+        >
+          {Object.values(recoveryEmail)}
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ handleClickDoneRecipes }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ handleClickFavoriteRecipes }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClickLogout }
+        >
+          Logout
+        </button>
       </div>
       <Footer />
     </div>
