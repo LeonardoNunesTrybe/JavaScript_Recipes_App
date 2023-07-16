608b324841b764bc8fba63bf1d25fae5596895ce
Jenifer <jnf.asm@gmail.com>
2023-07-16 12:52:39 +0100

    Profile: req 57 e 58

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,9 +3,16 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default function Profile() {
+  const recoveryEmail = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
       <Header haveBar={ false } title="Profile" />
+      <div>
+        <p data-testid="profile-email">{Object.values(recoveryEmail)}</p>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </div>
   );
